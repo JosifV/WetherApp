8da452a32f3588281a4a783a2cf6f84a58fddb3d
Josif <josifvacic@gmail.com>
2019-05-17 13:25:26 +0200

    message

---
+++ client/build/precache-manifest.d7a9d43ce483a7b46b7d66ae5831df3f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0c7b1177fdc812964fe92cc82b291c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c2fc92c124b273ab2b",
+    "url": "/static/css/2.a64ed4af.chunk.css"
+  },
+  {
+    "revision": "841292442bb9e35fd406",
+    "url": "/static/css/main.9e4e1a32.chunk.css"
+  },
+  {
+    "revision": "e8c2fc92c124b273ab2b",
+    "url": "/static/js/2.1a8e914f.chunk.js"
+  },
+  {
+    "revision": "841292442bb9e35fd406",
+    "url": "/static/js/main.b78cd388.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
